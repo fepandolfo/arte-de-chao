17cc04a309048ec914fc1b4cd1ff4089f7a7f7ca
Felipe <lf.pandolfo88@gmail.com>
2026-06-08 14:51:04 -0300

    Correção no botão RETORNAR AO INICIO

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -245,12 +245,9 @@ export default function ProductDetail() {
       </section>
 
       <footer className="border-t border-ash/40 px-6 lg:px-16 py-12 text-center">
-        <a
-          href="/#do-chao"
-          className="label-eyebrow editorial-link"
-        >
+        <Link to="/#do-chao" className="label-eyebrow editorial-link">
           ← Retornar ao Início
-        </a>
+        </Link>
       </footer>
     </div>
   );
